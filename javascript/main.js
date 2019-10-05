c65797e225a5af1c463e784692796b2457f2bd2b
Nico van Zyl <nico@wixelhq.com>
2019-10-05 18:21:37 +0200

    Adding form section

--- javascript/main.js
+++ javascript/main.js
@@ -6,16 +6,22 @@ var $header = document.getElementById("#header");
 var $nav = document.getElementById("#nav");
 var $footer = document.getElementById("#footer");
 // Helpers
-var /**
-   * @description Test if the body id is something
-   * @param  		{string}
-   * @return 		{bool}
-   *
-   */
-  page = function page(name) {
-    if (!name) {
-      return $body.getAttribute("id");
-    }
+var
+/**
+* @description Test if the body id is something
+* @param  		{string}
+* @return 		{bool}
+*
+*/
+page = function page(name) {
+  if (!name) {
+    return $body.getAttribute("id");
+  }
 
-    return $body.getAttribute("id") == name;
-  };
+  return $body.getAttribute("id") == name;
+};
+// add a class of 'intro' to the first paragraph
+// do this on every page apart from the blog page
+if (!page('blog')) {
+  document.querySelector('.content > p').classList.add('intro');
+}
