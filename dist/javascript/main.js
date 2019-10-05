80fa9b753ec95d58f25cd22ef10aec8d8d7d68a5
Nico van Zyl <nico@wixelhq.com>
2019-10-05 19:28:51 +0200

    Fix error

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -22,6 +22,7 @@ page = function page(name) {
 };
 // add a class of 'intro' to the first paragraph
 // do this on every page apart from the blog page
-if (!page('blog')) {
-  document.querySelector('.content > p').classList.add('intro');
-}
+// if ( !page('blog') )
+// {
+//     document.querySelector('.content > p').classList.add('intro')
+// }
