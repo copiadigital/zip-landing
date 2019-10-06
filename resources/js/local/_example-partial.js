9722fc0bad974f21723034a13a8a6e08da9dbf0e
Nico van Zyl <nico@wixelhq.com>
2019-10-06 18:04:37 +0200

    Add validation

--- resources/js/local/_example-partial.js
+++ resources/js/local/_example-partial.js
@@ -6,3 +6,29 @@
 //     document.querySelector('.content > p').classList.add('intro')
 
 // }
+window.onload = function() {
+  var form = document.getElementById("ctaForm");
+
+  let defaultConfig = {
+    // class of the parent element where the error/success class is added
+    classTo: "form-group",
+    errorClass: "text-red-700",
+    successClass: "text-green-700",
+    // class of the parent element where error text element is appended
+    errorTextParent: "form-group",
+    // type of element to create for the error text
+    errorTextTag: "div",
+    // class of the error text element
+    errorTextClass: "text-help"
+  };
+
+  // create the pristine instance
+  var pristine = new Pristine(form, defaultConfig);
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    // check if the form is valid
+    var valid = pristine.validate(); // returns true or false
+  });
+};
